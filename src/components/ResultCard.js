660b9894f436a8052c76775205a92f2fab894cf6
Duley Williams <duleygrafix@gmail.com>
2022-12-13 01:23:44 -0600

    final

--- src/components/ResultCard.js
+++ src/components/ResultCard.js
@@ -3,12 +3,22 @@ import { GlobalContext } from '../context/GlobalState';
 
 export const ResultCard = ({movie}) => {
     const {
-        addMovieToWatchlist, watchlist
+        addMovieToWatchlist,
+        watchlist,
+        watched,
+        addMovieToWatched,
     } = useContext(GlobalContext);
 
     let storedMovie = watchlist.find(o => o.id === movie.id);
+    let storedMovieWatched = watched.find((o) => o.id === movie.id);
 
-    const watchlistDisabled = storedMovie ? true : false;
+    const watchlistDisabled = storedMovie
+        ? true 
+        : storedMovieWatched
+        ? true
+        : false;
+
+    const watchedDisabled = storedMovieWatched ? true : false;
 
   return (
     <div className='result-card'>
@@ -36,6 +46,14 @@ export const ResultCard = ({movie}) => {
                     onClick={() => addMovieToWatchlist(movie)}
                 >
                     Add to Watchlist
+                </button>
+
+                <button
+                    className='btn'
+                    disabled={watchedDisabled}
+                    onClick={() => addMovieToWatched(movie)}
+                >
+                    Add to Watched
                 </button>
             </div>
         </div>
